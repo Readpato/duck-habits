b8ce65c90d2b07d686bf00a9b9aa918e05703cc5
Patrick Raedler <patoraedler@gmail.com>
2023-07-30 17:16:46 +0200

    feat(App): add initial logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,48 @@
+import { useEffect, useState } from 'react'
+
+interface Date {
+  month: string
+  year: number
+}
+
 function App() {
-  return <h1>Duck habits</h1>
+  const [date, setDate] = useState<Date>(null)
+  const getTime = () => {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ]
+    const month = months[new Date().getMonth()]
+    const year = new Date().getFullYear()
+    setDate({ month, year })
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      getTime()
+    }, 5000)
+  }, [])
+
+  return (
+    <main>
+      <h1>Duck habits</h1>
+      <button onClick={getTime}>Click me</button>
+      <p>
+        {date === null && 'Loading'}
+        {date !== null && `Year: ${date.year}, Month: ${date.month}`}
+      </p>
+    </main>
+  )
 }
 
 export default App
