f3e9054832f924f5f39cc528d46a635c9944e292
Patrick Raedler <patoraedler@gmail.com>
2023-07-31 23:37:23 +0200

    refactor(App): add quantity of days of month:

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,43 @@
 import { useEffect, useState } from 'react'
 
 interface Date {
+  days: number
   month: string
   year: number
 }
 
+interface Month {
+  days: number
+  id: number
+  name: string
+}
+
 function App() {
-  const [date, setDate] = useState<Date>(null)
+  const [date, setDate] = useState<Date | null>(null)
   const getTime = () => {
-    const months = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
-      'July',
-      'August',
-      'September',
-      'October',
-      'November',
-      'December',
+    const months: Month[] = [
+      { id: 0, name: 'January', days: 31 },
+      { id: 1, name: 'February', days: 28 },
+      { id: 2, name: 'March', days: 31 },
+      { id: 3, name: 'April', days: 30 },
+      { id: 4, name: 'May', days: 31 },
+      { id: 5, name: 'June', days: 30 },
+      { id: 6, name: 'July', days: 31 },
+      { id: 7, name: 'August', days: 31 },
+      { id: 8, name: 'September', days: 30 },
+      { id: 9, name: 'October', days: 31 },
+      { id: 10, name: 'November', days: 30 },
+      { id: 11, name: 'December', days: 31 },
     ]
-    const month = months[new Date().getMonth()]
+    const month = months.find(
+      (month) => month.id === new Date().getMonth()
+    ) as Month
     const year = new Date().getFullYear()
-    setDate({ month, year })
+    setDate({ month: month.name, days: month.days, year })
   }
 
   useEffect(() => {
-    setTimeout(() => {
-      getTime()
-    }, 5000)
+    getTime()
   }, [])
 
   return (
@@ -39,7 +46,8 @@ function App() {
       <button onClick={getTime}>Click me</button>
       <p>
         {date === null && 'Loading'}
-        {date !== null && `Year: ${date.year}, Month: ${date.month}`}
+        {date !== null &&
+          `Year: ${date.year}, Month: ${date.month}, Days: ${date.days}`}
       </p>
     </main>
   )
