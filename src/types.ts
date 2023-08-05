a8664800ae0b91c8a49d40e15f952277b62e8ed7
Patrick Raedler <patoraedler@gmail.com>
2023-08-05 22:53:05 +0200

    feat(types): create file to centralize types

--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,16 @@ export interface Habit {
   id: number
   text: string
 }
+
+// Date related
+export interface Month {
+  days: number
+  id: number
+  name: string
+}
+
+export interface Date {
+  days: number
+  month: string
+  year: number
+}
